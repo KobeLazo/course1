9f039da0af5beaccf8aa5d3547e6aa136d90737b
KobeLazo <422004424@ntc.edu.ph>
2025-12-11 18:42:23 -0800

    Update data.h

--- include/common/data.h
+++ include/common/data.h
@@ -5,8 +5,8 @@
  * This header file provides an abstraction of the basic data manipulation,
  * converting data from int to ASCII, and vice-versa
  *
- * @author Mohammed Abdelalim
- * @date 24/10/2020
+ * @author Kobe Michael Lazo
+ * @date 12/12/2025
  *
  */
 
